f7cfb107bbc1e976aad6d9ffd5609ccab36df479
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-10-16 13:17:19 -0400

    10/16

---
+++ Shapess/src/Rectangle.java
@@ -0,0 +1,12 @@
+
+public class Rectangle implements Shape{
+	private double length;
+	private double width;
+	
+	public double perimeter()
+	{
+		return length*2 + width*2;
+	}
+	
+	public double
+}
