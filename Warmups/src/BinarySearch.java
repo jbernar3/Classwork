62193c134d92fd84470f196db03801298eab823b
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-11-15 12:07:57 -0500

    kjasdl;kfj;welfhlqwejifhawefhlqweif;ioweqjfojserao;ghiqwoerihtopqweihfpowiqfoewl;gkih;roith;woiht;oqweijo;weqithjg;owqihet;oaisto;iwhjeo;ithjqwo;eihjhjhjhjhjhjhjas;otiqwo;eirth;woiehjwo;qieu;wieu;owuie;litrj;shieqwie;lwiqu;elithwe;lithwileht

--- Warmups/src/BinarySearch.java
+++ Warmups/src/BinarySearch.java
@@ -1,6 +1,7 @@
 public class BinarySearch {
 	public static void main (String[] args)
 	{
+	/*
 		int[] test1 = {1,2,3,4,5,6,7,8,9,10};
 		int[] test2 = {0,4,5,6,12,15,17,23,29};
 		
@@ -8,6 +9,22 @@ public class BinarySearch {
 		System.out.println(binarySearch(test2, 23, 0, test2.length)); //7
 		System.out.println(binarySearch(test1, 1, 0, test1.length)); //0
 		System.out.println(binarySearch(test2, 25, 0, test2.length)); //-1, this may cause an error.
+	*/
+	
+		int[] test3 = {0,1,2,3,4,5,6,7,8,9};
+		int[] test4 = {0,1,2,3,4,5,6,7,8};
+	
+		for (int i = -1; i<test3.length; i++)
+		{
+			System.out.print(binarySearch(test3, i, 0, test3.length));
+		}
+		System.out.println();
+		
+		for (int i = -1; i<test4.length; i++)
+		{
+			System.out.print(binarySearch(test4, i, 0, test4.length));
+		}
+		System.out.println();
 	}
 	
 	public static int binarySearch(int[] nums, int query, int first, int last)
