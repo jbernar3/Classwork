e607ca8a6b0438764d959c904c2563b65e8e3984
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-03 12:29:52 -0500

    asdf

--- ClassLabs/src/Lab4_3$EASY/GameCodeEasy.java
+++ ClassLabs/src/Lab4_3$EASY/GameCodeEasy.java
@@ -6,9 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-import java.awt.Label;
+
 import java.io.File;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.io.FileInputStream;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -16,6 +18,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 
 public class GameCodeEasy extends Application{
 
@@ -28,20 +31,31 @@ public class GameCodeEasy extends Application{
 		Button button = new Button("CLICK",imageView);
 		button.setMinSize(200, 225);
 		button.setMaxSize(300, 325);
+		int score = 0;
+		Label label = new Label(""+score);
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask()
+		{
+			public void run()
+			{
+				
+			}
+		};
 
-		//HBox hbox = new HBox(button);
-		//hbox.setAlignment(Pos.CENTER);
-		//hbox.setPrefSize(20, 20);
+		HBox hbox = new HBox(button,label);
+		hbox.setAlignment(Pos.CENTER);
+		hbox.setPrefSize(20, 20);
 		button.setOnAction(value -> {
-			button.setLayoutX(Math.random()*600+50);
-			button.setLayoutY(Math.random()*600+50);
+			hbox.setTranslateX(Math.random()*600-300);
+			hbox.setTranslateY(Math.random()*600-300);
+			//score++;
+			label.setText(""+score);
 		});
 		StackPane root = new StackPane();
-		root.getChildren().add(button);
+		root.getChildren().add(hbox);
+		root.getChildren().add(label);
 		primaryStage.setScene(new Scene(root, 800, 800));
-		
 		primaryStage.show();
-		
 	}
 	
 	public static void main(String[] args)
