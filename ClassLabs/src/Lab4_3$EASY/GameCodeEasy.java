fd607f8c940d6634efb2c7db2994f81738df490c
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-03 13:13:15 -0500

    asdfasdf

--- ClassLabs/src/Lab4_3$EASY/GameCodeEasy.java
+++ ClassLabs/src/Lab4_3$EASY/GameCodeEasy.java
@@ -1,5 +1,6 @@
 package Lab4_3$EASY;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Node;
@@ -21,43 +22,68 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 
 public class GameCodeEasy extends Application{
+	int score = 0;
+	boolean gameOn = false;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Bron Clicker");
+		
 		FileInputStream input = new FileInputStream("lebron3.png");
 		Image image = new Image(input);
 		ImageView imageView = new ImageView(image);
 		Button button = new Button("CLICK",imageView);
 		button.setMinSize(200, 225);
 		button.setMaxSize(300, 325);
-		int score = 0;
+		
 		Label label = new Label(""+score);
 		Button startButton = new Button("START",label);
-	
-		Timer timer = new Timer();
-		TimerTask task = new TimerTask()
-		{
-			public void run()
-			{
-				
-			}
-		};
-
+		startButton.setAlignment(Pos.TOP_LEFT);
+		
 		HBox hbox = new HBox(button,label);
 		hbox.setAlignment(Pos.CENTER);
 		hbox.setPrefSize(20, 20);
+		
+		//ANIMATION TIMER
+		new AnimationTimer()
+		{
+			public void handle(long now)
+			{
+				//if ()
+			}
+		};
+		
+		//START BUTTON ACTION
+		startButton.setOnAction(value-> {
+			gameOn = !gameOn;
+			if (gameOn)
+			{
+				startButton.setText("STOP");
+			}
+			else
+			{
+				startButton.setText("START");
+			}
+			
+		});
+		
+		//PICTURE BUTTON ACTION
 		button.setOnAction(value -> {
-			hbox.setTranslateX(Math.random()*600-300);
-			hbox.setTranslateY(Math.random()*600-300);
-			//score++;
+			
+			if (gameOn)
+			{
+				hbox.setTranslateX(Math.random()*600-300);
+				hbox.setTranslateY(Math.random()*600-300);
+				score++;
+			}
 			label.setText(""+score);
 		});
+		
 		StackPane root = new StackPane();
 		root.getChildren().add(hbox);
 		root.getChildren().add(startButton);
-		startButton.setTranslateX(-400);
-		startButton.setTranslateY(400);
+		startButton.setTranslateX(-40);
+		startButton.setTranslateY(40);
 		primaryStage.setScene(new Scene(root, 800, 800));
 		primaryStage.show();
 	}
